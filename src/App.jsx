6e2cb10c43165a227cd231dcafeeeb888fc622b7
ishan-web-dev <joshiishaan86@gmail.com>
2024-12-24 23:34:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import menu from './components/menu'
+
+function App() {
+  return (
+    <div className='App'>
+      <menu/>
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from "../img/logo.png"
+
+function menu() {
+  return (
+    <menu>
+        <img src={Logo} alt="" />
+        
+    </menu>
+  )
+}
+
+export default menu;
